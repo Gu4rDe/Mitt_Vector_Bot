5e3f3b91bb55e92b4d836da91b563a5df08efe41
GuArDe <gu4rde@gmail.com>
2025-12-20 12:52:14 +0300

    Добавлено значение для идентификатора и URL отчёта в словарях FILES_ID и FILES_URL

--- consts.py
+++ consts.py
@@ -4,14 +4,14 @@
 FILES_ID = {
     "презентация": "1O1e42ewsS7PpToHdAM3Q3FahKU7hKWSl",
     "паспорт": "",  # not used
-    "отчет": "",  # not used
+    "отчёт": "1t0hT5y-gy3fiGYKFpK5YKrGLS0XdmLac",
 }
 FILES_URL = {
     "презентация": "https://docs.google.com/presentation/d/1O1e42ewsS7PpToHdAM3Q3FahKU7hKWSl/edit?usp=drive_link&ouid=110813979345823164835&rtpof=true&sd=true",
+    "отчёт": "https://docs.google.com/document/d/1t0hT5y-gy3fiGYKFpK5YKrGLS0XdmLac/edit?usp=drive_link&ouid=110813979345823164835&rtpof=true&sd=true",
     "тест": "https://docs.google.com/forms/d/e/1FAIpQLSf_Dof83g66gSKwULu-3fBtt5P1rB4OmSKZ8U4mQLauUFQEYg/viewform",
     "паспорт": "",  # not used
     "курс": "https://acc-41784.ispring.ru/app/preview/a28a63d8-d84e-11f0-ba07-e22299192c55",
-    "отчет": "https://docs.google.com/document/d/1t0hT5y-gy3fiGYKFpK5YKrGLS0XdmLac/edit?usp=drive_link&ouid=110813979345823164835&rtpof=true&sd=true",  # not used
 }
 TEAM_INFO = '''
 • Губин Артём - капитан
