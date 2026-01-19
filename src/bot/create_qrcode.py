8b2e0c8f88a1f73771fd39a3cc96cd1f6a41ea3e
GuArDe <gu4rde@gmail.com>
2026-01-19 13:27:07 +0300

    Добавлена проверка на наличие папки

--- src/bot/create_qrcode.py
+++ src/bot/create_qrcode.py
@@ -1,10 +1,13 @@
+import os
 import segno
 from .constants import FILES_URL
 
 
 def create_qrcode(file_name: str):
     file_path = f"data/qrcodes/{file_name}.png"
+    folder_path = os.path.dirname(file_path)
     try:
+        os.makedirs(folder_path, exist_ok=True)
         return open(file_path, "rb")
     except FileNotFoundError:
         qrcode = segno.make_qr(FILES_URL[f"{file_name}"])
--- src/bot/download_file.py
+++ src/bot/download_file.py
@@ -1,11 +1,14 @@
+import os
 from googledrivedownloader import download_file_from_google_drive
 from .constants import FILES_ID
 
 
 def download_file(file_name: str, extension: str):
-
     file_path = f"data/files/{file_name}.{extension}"
+    folder_path = os.path.dirname(file_path)
+
     try:
+        os.makedirs(folder_path, exist_ok=True)
         return open(file_path, "rb")
 
     except FileNotFoundError:
